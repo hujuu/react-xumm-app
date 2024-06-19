cca8da0d9e62031c17920a885993109a7b145ead
hujuu <1961946+hujuu@users.noreply.github.com>
2024-06-19 23:04:59 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -37,31 +37,27 @@ export default function App() {
     };
 
     return (
-        <>
-            <div className="relative w-full h-[350px]">
-                <div
-                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-lg text-stone-50">
-                    {!account && (
-                        <div
-                            className={"mt-3 btn btn-wide"}
-                            onClick={connect}>
-                            ウォレットを接続してはじめる
+        <div className="relative w-full h-[350px]">
+            <div
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-lg text-stone-50">
+                {!account && (
+                    <div
+                        className={"mt-3 btn btn-wide"}
+                        onClick={connect}>
+                        ウォレットを接続してはじめる
+                    </div>
+                )}
+                {account && (
+                    <div>
+                        <div className={"mt-3 text-gray-800"}>
+                            Account ID: {account}
                         </div>
-                    )}
-                    {account && (
-                        <div>
-                            <div
-                                className={"mt-3 btn btn-wide"}
-                                onClick={connect}>
-                                Account ID: {account}
-                            </div>
-                            <div>
-                                {balance !== undefined ? `XRP Balance: ${Number(balance)/1000000}` : 'Fetching balance...'}
-                            </div>
+                        <div className={"mt-3 text-gray-800"}>
+                            {balance !== undefined ? `XRP Balance: ${Number(balance)/1000000}` : 'Fetching balance...'}
                         </div>
-                    )}
-                </div>
+                    </div>
+                )}
             </div>
-        </>
+        </div>
     );
 }
