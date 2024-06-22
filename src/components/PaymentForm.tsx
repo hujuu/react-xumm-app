814687e487ba68dcf80fa8c8c70de729b6b3eac5
hujuu <1961946+hujuu@users.noreply.github.com>
2024-06-23 05:31:32 +0900

    labelのhtmlForをinputのnameと一致させる

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -45,13 +45,12 @@ export default function PaymentForm({ account }: { account: string | undefined }
                             name="wallet-address"
                             id="wallet-address"
                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            placeholder=""
                             onChange={(e) => setDestinationAddress(e.target.value)}
                         />
                     </div>
                     <div className="relative mb-4">
                         <label
-                            htmlFor="name"
+                            htmlFor="amount"
                             className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
                         >
                             送金額
